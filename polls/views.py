044ce233648af7f271819881be47f6b2be050d29
Ana Rey <anarey@gmail.com>
2015-09-08 09:57:25 +0200

    polls: view: No listar las preguntas que no están publicadas
    Mejoras en la view, para que a la hora de listas las ultimas preguntas,
    omita las preguntas que tienen como fecha de publicación en el futuro.

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -12,7 +13,10 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        now = timezone.now()
+        return Question.objects.filter(
+            pub_date__lte = now
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
